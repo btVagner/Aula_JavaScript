e1f08e6051ab40c9f724f5dd201da0fbcafdceb6
btVagner <bruno.tomv@gmail.com>
2021-05-29 17:09:05 -0300

    finalizando introdução ao JS

---
+++ JS/main.js
@@ -0,0 +1,82 @@
+// function clicou(){
+//     document.getElementById("agradecimento").innerHTML="<b>Thanks for click</b>";
+//     // console.log(document.getElementById("agradecimento"));
+//     // alert("Thanks for click");
+// }
+// function redirecionar(){
+//     window.open("https://google.com/");
+//     // window.location.href = "https://google.com/";
+// }
+// function trocar(elemento){
+//     elemento.innerHTML="Thanks for mouse over"
+//     // alert ("Trocar texto");
+// }
+// function voltar(elemento){
+//     elemento.innerHTML="Mouse over"
+// }
+// function load(){
+//     alert("Página carregada");
+// }
+
+// function funcaoChange(elemento){
+//     console.log(elemento.value);
+// }
+
+// function soma(n1,n2){
+//     return n1+n2;
+// }
+// alert(soma(5,10));
+
+// var validar = 0;
+// function validaIdade(idade){
+//     var validar;
+//     if(idade >= 18){
+//         validar = true;
+//     }
+//     else{
+//         validar = false;
+//     }
+//     return (validar);
+// }
+// var idade = prompt("Qual sua idade?");
+// console.log(validaIdade(idade));
+
+// var d = new Date();
+// alert(d.getDate());
+// alert(d.getHours());
+// alert(d.getMinutes());
+
+// var count;
+// for(count=0; count<=5; count++){
+//     console.log(count);
+// }
+
+// var count = 0;
+// while(count<=5){
+//     console.log(count);
+//     count++;
+// }
+
+// var idade = prompt("Qual sua idade?");
+// if (idade >= 18){
+//     alert("Maior de idade");
+// }
+// else{
+//     alert("Menor de idade");
+// }
+
+// var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+// var fruta = {nome: "maça", cor:"vermelha"};
+// console.log(fruta.cor);
+// alert(fruta.cor);
+
+// var lista=["maça", "pera", "laranja"];
+// lista.push("uva");
+// lista.pop()
+
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
